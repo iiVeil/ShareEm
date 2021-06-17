d2dd6ad5246bb52d9f05184227debaecc8bd6d24
iiVeil <52388052+iiVeil@users.noreply.github.com>
2021-06-17 16:50:47 -0400

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using HarmonyLib;
+using NetworkHelper;
+using System;
+using System.IO;
+using System.Reflection;
+
+
+namespace ShareEm
+{
+    [BepInPlugin(Id, "ShareEm", Version)]
+    public class Main : BaseUnityPlugin
+    {
+        #region[Declarations]
+
+        public const string
+            MODNAME = "$safeprojectname$",
+            AUTHOR = "",
+            GUID = AUTHOR + "_" + MODNAME,
+            VERSION = "1.0.0.0";
+
+        internal readonly ManualLogSource log;
+        internal readonly Harmony harmony;
+        internal readonly Assembly assembly;
+        public readonly string modFolder;
+
+        #endregion
+
+
+        public const string Id = "mod.iiveil.ShareEm";
+        public const string Version = "1.0.0";
+        public const string Name = "ShareEm";
+        public static Session session = new Session(Id);
+        public Main()
+        {
+            
+            log = Logger;
+            harmony = new Harmony(Id);
+            assembly = Assembly.GetExecutingAssembly();
+            modFolder = Path.GetDirectoryName(assembly.Location);
+        }
+
+        public void Start()
+        {
+            harmony.PatchAll(assembly);
+            
+        }
+    }
+}
---
+++ Patches/Patches.cs
@@ -0,0 +1,112 @@
+﻿using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+using TMPro;
+using System;
+using PacketHelper;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using Steamworks;
+using Steamworks.Data;
+
+namespace ShareEm.Patches
+{
+    [HarmonyPatch]
+    class ShareEm
+    {
+
+
+        /*
+         * Create new packets.
+         */
+        public static Session session = new Session(Main.Id);
+        private static BindingFlags flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
+        [HarmonyPatch(typeof(Server), "InitializeServerPackets")]
+        [HarmonyPostfix]
+        static void CreateNewServerPackets()
+        {
+            session.CreateNewServerPacket("ServerHandleShare", ServerHandleShare);
+        }
+
+        [HarmonyPatch(typeof(LocalClient), "InitializeClientData")]
+        [HarmonyPostfix]
+        static void CreateNewClientPackets()
+        {
+            session.CreateNewClientPacket("ClientHandleShare", ClientHandleShare);
+        }
+
+
+
+
+        /*
+         * Send share packet when a player picks up an item
+         */
+        [HarmonyPatch(typeof(ClientSend), "PickupItem")]
+        [HarmonyPrefix]
+        static void ClientSendSharePacket(ClientSend __instance, int itemID)
+        {
+            Item item = ItemManager.Instance.list[itemID].GetComponent<Item>();
+            if (item.powerup)
+            {
+                Data data = new Data();
+                data.ints = new int[] { item.powerup.id };
+                MonoBehaviour.print($"Sending ClientSharePacket from {LocalClient.instance.myId}");
+                session.SendPacketToServer("ServerHandleShare", data);
+            }
+        }
+
+
+        // Share to the client
+        static void ClientHandleShare(Packet packet)
+        {
+            Debug.Log("Recieved share packet");
+            int fromClient = packet.ReadInt();
+            int itemID = packet.ReadInt();
+            string fromUsername = packet.ReadString();
+            Debug.Log($"{fromClient}, {itemID}");
+
+            if (fromClient == LocalClient.instance.myId)
+            {
+                ChatBox.Instance.AppendMessage(-1, $"<color=red>You <color=white>just shared <color={getPowerupByID(itemID).GetColorName()}>{getPowerupByID(itemID).name} <color=white>with everyone!", "");
+            } else
+            {
+                ChatBox.Instance.AppendMessage(-1, $"<color=red>{fromUsername} <color=white>just shared <color={getPowerupByID(itemID).GetColorName()}>{getPowerupByID(itemID).name} <color=white>with you!", "");
+                addPowerup(getPowerupByID(itemID));
+            }
+        }
+
+        static void ServerHandleShare(int fromClient, Packet packet)
+        {
+            int itemID = packet.ReadInt();
+            MonoBehaviour.print($"Received share packet from {fromClient}");
+            Data data = new Data();
+            data.ints = new int[] { fromClient, itemID };
+            data.strings = new string[] { Server.clients[fromClient].player.username };
+            MonoBehaviour.print($"Echoing share to all players.");
+            session.SendPacketToAllClients("ClientHandleShare", data);
+        }
+
+
+        // Add a powerup to the current client.
+        static void addPowerup(Powerup powerup)
+        {
+            UiEvents.Instance.AddPowerup(powerup);
+            PowerupUI.Instance.AddPowerup(powerup.id);
+            int[] currentPowerups = typeof(PowerupInventory).GetField("powerups", flags).GetValue(PowerupInventory.Instance) as int[];
+            currentPowerups[powerup.id]++;
+            PlayerStatus.Instance.UpdateStats();
+        }
+
+        static Powerup getPowerupByID(int id)
+        {
+            return ItemManager.Instance.allPowerups[id];
+        }
+
+
+
+    }
+}
